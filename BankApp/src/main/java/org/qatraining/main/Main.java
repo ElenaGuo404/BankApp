5f936d4370649c62eb035e87542792ba12897238
ElenaGuo404 <guojiaying2001@hotmail.com>
2024-10-30 10:10:48 +0000

    update package name

---
+++ BankApp/src/main/java/org/qatraining/main/Main.java
@@ -0,0 +1,21 @@
+package org.qatraining.main;
+
+import org.qatraining.account.BankAccount;
+import org.qatraining.account.CurrentAccount;
+import org.qatraining.account.SavingAccount;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        BankAccount[] bankAccount= new BankAccount[3];
+        bankAccount[0] = new BankAccount(1300, 1, "Elena" ) ;
+        bankAccount[1] = new CurrentAccount(1000, 1 ,"Elena", 500);
+        bankAccount[2] = new SavingAccount(300, 0, "Ann", 0.05);
+
+        for (BankAccount account : bankAccount){
+            System.out.println(account);
+        }
+
+    }
+}
---
+++ train3/src/main/java/org/example/abs/Dolphin.java
@@ -0,0 +1,2 @@
+package org.example.abs;public class Dolphin {
+}
